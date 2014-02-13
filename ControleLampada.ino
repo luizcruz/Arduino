2d902ae79bed30a301992af5e42fba914088001a
System Administrator <root@MacBook-Pro-de-Luiz.local>
2014-02-13 17:07:39 -0200

    Controle Lampada

---
+++ ControleLampada.ino
@@ -0,0 +1,76 @@
+/*
+
+* Controlando a lampada por proximidade
+* Componentes: Luminaria de escritorio, Rele (Modulo Rele v.1.0 - MPA-S-105-C), Arduino Duemilanove ou equivalente e sensor ultrassom (HC-SR04)
+* Autor: Luiz Cruz (Revista INFO)
+* 
+*/
+
+
+//Variaveis que definem os pinos de conexao
+int trigPin = 5;
+int echoPin = 6;
+int relePin = 9;
+bool estado = false;
+
+// Variaveis para calculo da distancia
+long duration, inches, x;
+int i;
+
+void setup(){
+  
+  // Inicializacao
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(relePin, OUTPUT);
+    
+}
+
+void loop(){
+  
+  // O ultrasom inicializa com pulso em alta por 2 ou mais microsegundos 
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, LOW);
+
+  // Duracao da ida e volta do sinal resulta no calculo da distancia 
+  duration = pulseIn(echoPin, HIGH);
+  x = microsecondsToCentimeters(duration);
+  
+  // Logica de ativacao
+  if (x < 30) {
+    
+    //Distancia menor que 30 cm, verifica estado
+    
+    if (estado == 0){
+        //Desliga lampada
+        digitalWrite(relePin, HIGH);
+        Serial.print(estado);
+        estado = true;
+    }
+    else{
+        // Liga a lampada
+        digitalWrite(relePin, LOW); 
+        Serial.print(estado);
+        estado = false;       
+    }
+     
+  }
+  
+  // Imprime no console a distancia (conferir ativacao do sensor ultrasom)
+  Serial.print("Distancia = ");
+  Serial.print(x);
+  Serial.print(" cm ");
+  Serial.println();
+  delay(500);
+}
+
+
+long microsecondsToCentimeters(long microseconds){
+ 
+  // Velocidade do som e 360 m/s ou 29 microsegundos por centimetro. A distancia e metade do percurso. 
+  return microseconds / 29 / 2;
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Arduino
 =======
 
 Repositório de códigos de projetos com Arduino
+
+*ControleLampada.ino  
+  - Lampada controlada por relé e sensor de ultrasom
